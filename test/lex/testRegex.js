76313aab3cf4c449fe408e244b700f5fc1550b91
Matt Bierner <mattbierner@gmail.com>
2013-09-12 20:19:20 -0400

    Initial work getting parsing of divs and regexpr working

--- test/lex/testRegex.js
+++ test/lex/testRegex.js
@@ -1,7 +1,8 @@
 define(['parse/parse', 'ecma/lex/regular_expression_lexer'], function(parse, regular_expression_lexer){
     
     var reEq = function(x, y) {
-        return assert.equal(x.toString(), y.toString());
+        assert.equal(x.body, y.body);
+        assert.equal(x.flags, y.flags);
     };
     
     return {
@@ -9,26 +10,26 @@ define(['parse/parse', 'ecma/lex/regular_expression_lexer'], function(parse, reg
         'tests': [
             ["Basic Regex",
             function(){
-                reEq(parse.run(regular_expression_lexer.regularExpressionLiteral, "/abc/"), /abc/);
+                reEq(parse.run(regular_expression_lexer.regularExpressionLiteral, "/abc/"), {'body': 'abc', 'flags':''});
                 
-                reEq(parse.run(regular_expression_lexer.regularExpressionLiteral, "/abc/i"), /abc/i);
+                reEq(parse.run(regular_expression_lexer.regularExpressionLiteral, "/abc/i"), {'body': 'abc', 'flags': 'i'});
                 
-                reEq(parse.run(regular_expression_lexer.regularExpressionLiteral, "/abc/igm"), /abc/igm);
+                reEq(parse.run(regular_expression_lexer.regularExpressionLiteral, "/abc/igm"), {'body': 'abc', 'flags': 'igm'});
             }],
             ["Simple Class",
             function(){
-                reEq(parse.run(regular_expression_lexer.regularExpressionLiteral, "/[abc]/"), /[abc]/);
+                reEq(parse.run(regular_expression_lexer.regularExpressionLiteral, "/[abc]/"), {'body':'[abc]', 'flags': ''});
                 
-                reEq(parse.run(regular_expression_lexer.regularExpressionLiteral, "/[^*. 3][]/"), /[^*. 3][]/);
+                reEq(parse.run(regular_expression_lexer.regularExpressionLiteral, "/[^*. 3][]/"), {'body':'[^*. 3][]', 'flags': ''})
             }],
             ["Escape Class",
             function(){
-                reEq(parse.run(regular_expression_lexer.regularExpressionLiteral, "/[\\]]/"), /[\]]/);
+                reEq(parse.run(regular_expression_lexer.regularExpressionLiteral, "/[\\]]/"), {'body':'[\\]]', 'flags': ''})
                 
             }],
             ["Escape Slash",
             function(){
-                reEq(parse.run(regular_expression_lexer.regularExpressionLiteral, "/\\/ab/"), /\/ab/);
+                reEq(parse.run(regular_expression_lexer.regularExpressionLiteral, "/\\/ab/"), {'body':'\\/ab', 'flags':''});
                 
             }],
            
